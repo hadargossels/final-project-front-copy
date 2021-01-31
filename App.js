6e78173f1211b433bdae9eeaed147339327ce5c9
lielsh <liel_sharon@outlook.com>
2021-01-31 21:26:10 +0200

    Task 1 - React

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
+import Middle from './components/Middle';
+import Footer from './components/Footer';
+
 class App extends Component{
    render(){
       return(
          <div>
-            <h1>Hello World</h1>
+            <Header />
+            <Middle />
+            <Footer />
          </div>
       );
    }
 }
+
 export default App;
