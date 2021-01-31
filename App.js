93c0a0274a3962bc3695f078d4a0ced0d3e538c8
raz-botner <razbotner@hotmail.com>
2021-01-31 22:01:02 +0200

    done product

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+import Product from './components/product';
+
 
 class App extends Component{
    render(){
       return(
          <div>
             <Header/>
-            <h1>Hello World</h1>
+            <Product/>
          </div>
       );
    }
