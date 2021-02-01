c27be9d393eb715449958da82286a06a1a055dbf
izhchako <rachel16188@gmail.com>
2021-02-01 11:46:05 +0200

    moved project to create react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from './components/Header/Header.js' 
+import Footer from './components/Footer/Footer.js' 
+import ProductPage from './components/ProductPage/ProductPage.js' 
+import './App.css';
+
+function App() {
+  return(
+    <div>
+       <Header/>
+       <ProductPage/>
+       <Footer/>
+    </div>
+ );
+}
+
+export default App;
--- src/components/ProductPage/ProductPage.js
+++ src/components/ProductPage/ProductPage.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import './productPage.css';
+
 class ProductPage extends Component{
    
    render(){
