fdc7e83588b3924da4a473a1b64ccd56d0ec9547
Mansor Mansor <mansor.m000@gmail.com>
2021-02-03 11:22:42 +0200

    Product page is done.

--- src/components/layout/Rating.js
+++ src/components/layout/Rating.js
@@ -3,19 +3,46 @@ import { Button } from 'react-bootstrap';
 import ReactStars from "react-rating-stars-component";
 
 export default class Rating extends Component {
+    constructor() {
+        super();
+        this.state = {
+            selectColor : "",
+            rating : 0,
+            numToBuy : 0
+        };
+    }
+
+    changeMyColor() {
+        return this.state.selectColor;
+    }
+
     render() {
         const rangeStyle = {
             width: '2.5vw',
             marginRight: '10px'
         }
+        const myColor = "Color selected: " + this.changeMyColor();
+        const colorChoose = (e) => {
+            this.setState({ selectColor : e.target.value });
+        }
         const ratingChanged = (newRating) => {
-            console.log(newRating);
+            this.setState({ rating : newRating })
         }
         const amountChanged = (newAmount) => {
-            console.log(newAmount.target.value);
+            this.setState({ numToBuy : newAmount.target.value })
         }
         return (
             <div>
+                <select
+                    value={this.state.selectColor}
+                    onChange={colorChoose}
+                >
+                    <option value="none">Choose your color</option>
+                    <option value="Cosmic Grey">Cosmic Grey</option>
+                    <option value="Cosmic Black">Cosmic Black</option>
+                    <option value="Cloud White">Cloud Whitek</option>
+                </select>
+                <p>{myColor}</p>
                 <input type="number" defaultValue="1" min="1" onChange={amountChanged} style={rangeStyle} />
                 <Button variant="secondary" id="addToBasketID" style={{ borderRadius: '20%', margin: '10px 0' }}>Add to basket</Button>{' '}
                 <div style={{ margin: '20px 37%' }}>
