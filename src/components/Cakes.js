860d51dbf26d76f0b5c840b7022f81c12a6d300f
raz-botner <razbotner@hotmail.com>
2021-02-04 15:22:54 +0200

    almost done filter

--- src/components/Cakes.js
+++ src/components/Cakes.js
@@ -5,34 +5,36 @@ import SortBar from "./Sortbar";
 
 
 const cakeArr = [
-   { img: '/images/cake1-1.jpeg', alt: 'PHOTO', title: 'טארט פירות העונה',priceSmall: 30,priceBig: 140, milk: true ,parve: false ,fruit: true, gluten: true,suger: true,stars: 5},
-   { img: '/images/cake2-1.jpeg', alt: 'PHOTO', title: 'פיסטוק-פירות יער',priceSmall: 35,priceBig: 180, milk: true ,parve: false ,fruit: true, gluten: true,suger: true,stars: 4},
-   { img: '/images/cake3-1.jpeg', alt: 'PHOTO', title: 'לוזיק- עוגת אגוזי לוז',priceSmall: 30,priceBig: 130, milk: true ,parve: false ,fruit: false, gluten: true,suger: true,stars: 3}, 
-   { img: '/images/cake4-1.jpeg', alt: 'PHOTO', title: 'עוגת גבינה קרמית',priceSmall: "",priceBig: 160, milk: true ,parve: false ,fruit: true, gluten: true,suger: true,stars: 3 },
-   { img: '/images/cake5-1.jpeg', alt: 'PHOTO', title: 'פאי לימון',priceSmall: "",priceBig: 120, milk: true ,parve: false ,fruit: true, gluten: false,suger: true,stars: 4 },
-   { img: '/images/cake6-1.jpeg', alt: 'PHOTO', title: 'עוגות שמרים',priceSmall: "",priceBig: 50, milk: true ,parve: true ,fruit: false, gluten: true,suger: true,stars: 5},
-   { img: '/images/cake7-1.jpeg', alt: 'PHOTO', title: 'עוגיות',priceSmall: "",priceBig: 50, milk: true ,parve: false ,fruit: true, gluten: true,suger: false,stars: 2},
-   { img: '/images/cake8-1.jpeg', alt: 'PHOTO', title: 'מקרונים',priceSmall: "",priceBig: 20, milk: true ,parve: false ,fruit: true, gluten: false,suger: true,stars: 3 },
-   { img: '/images/cake9-1.jpeg', alt: 'PHOTO', title: 'חלות שבת',priceSmall: "",priceBig: 10, milk: false ,parve: true ,fruit: false, gluten: true,suger: false,stars: 5 },
+   { img: '/images/cake1-1.jpeg', alt: 'PHOTO',id: 1, title: 'טארט פירות העונה',priceSmall: 30,priceBig: 140, milk: true ,parve: false ,fruit: true, glutenFree: false,shugerFree: false,stars: 5},
+   { img: '/images/cake2-1.jpeg', alt: 'PHOTO',id: 2, title: 'פיסטוק-פירות יער',priceSmall: 35,priceBig: 180, milk: true ,parve: false ,fruit: true, glutenFree: false,shugerFree: false,stars: 4},
+   { img: '/images/cake3-1.jpeg', alt: 'PHOTO',id: 3, title: 'לוזיק- עוגת אגוזי לוז',priceSmall: 30,priceBig: 130, milk: true ,parve: false ,fruit: false, glutenFree: false,shugerFree: false,stars: 3}, 
+   { img: '/images/cake4-1.jpeg', alt: 'PHOTO',id: 4, title: 'עוגת גבינה קרמית',priceSmall: "",priceBig: 160, milk: true ,parve: false ,fruit: true, glutenFree: false,shugerFree: false,stars: 3 },
+   { img: '/images/cake5-1.jpeg', alt: 'PHOTO',id: 5, title: 'פאי לימון',priceSmall: "",priceBig: 120, milk: true ,parve: false ,fruit: true, glutenFree: true,shugerFree: false,stars: 4 },
+   { img: '/images/cake6-1.jpeg', alt: 'PHOTO',id: 6, title: 'עוגות שמרים',priceSmall: "",priceBig: 50, milk: true ,parve: true ,fruit: false, glutenFree: false,shugerFree: false,stars: 5},
+   { img: '/images/cake7-1.jpeg', alt: 'PHOTO',id: 7, title: 'עוגיות',priceSmall: "",priceBig: 50, milk: true ,parve: false ,fruit: true, glutenFree: false,shugerFree: true,stars: 2},
+   { img: '/images/cake8-1.jpeg', alt: 'PHOTO',id: 8, title: 'מקרונים',priceSmall: "",priceBig: 20, milk: true ,parve: false ,fruit: true, glutenFree: true,shugerFree: false,stars: 3 },
+   { img: '/images/cake9-1.jpeg', alt: 'PHOTO',id: 9, title: 'חלות שבת',priceSmall: "",priceBig: 10, milk: false ,parve: true ,fruit: false, glutenFree: false,shugerFree: true,stars: 5 },
 ]
 
-const updateArr= cakeArr.reverse()
-
+const updateCakeArr=[...cakeArr.reverse()]
 
 class Cakes extends Component {
 
    constructor(){
       super()
       this.state={
-         Arr: [...cakeArr],
+         Arr: [...updateCakeArr],
          filterArr: [],
+         sortSelected:"",
       }
       this.sortFunc=this.sortFunc.bind(this)
       this.addFilter=this.addFilter.bind(this)
+      this.filtering=this.filtering.bind(this)
    }
 
    addFilter(e){
       let copyFilterArr=[...this.state.filterArr]
+
       if(e.checked){
          copyFilterArr.push(e.id)
       }else{
@@ -40,22 +42,56 @@ class Cakes extends Component {
             return (e.id !==item)
          })
       }
+      this.filtering(copyFilterArr)
       this.setState({filterArr: copyFilterArr})
    }
 
-   // filtering(){
-   //    let copyFilterArr=[...this.state.filterArr]
-   //    let copyArr=[...this.state.Arr]
 
-   //    for (const iterator of copyFilterArr) {
-   //       copyArr=copyArr.filter((item)=>{
-   //          return (iterator !==item.id)
-   //       })
-   //    }
-   //    this.setState({Arr: copyArr})
 
-   // }
+   filtering(copyFilterArr){
 
+      let copyArr=[...cakeArr]
+
+      let flagFruit=copyFilterArr.filter((item)=>{
+         return (item==="fruit" || item==="withoutfruit")
+      })
+
+      if(flagFruit.length==2){
+         copyFilterArr=copyFilterArr.filter((item)=>{
+            return (item !=="fruit" && item !=="withoutfruit")
+         })
+      }
+
+      for (const type of copyFilterArr) {
+         if(type==="price1"){
+            copyArr=copyArr.filter((item)=>{
+               return (item["priceBig"]<=100)
+            })
+         }
+         else if(type==="price2"){
+            copyArr=copyArr.filter((item)=>{
+               return (item["priceBig"]>=100 && item["priceBig"]<=200)
+            })
+         }
+         else if(type==="price3"){
+            copyArr=copyArr.filter((item)=>{
+               return (item["priceBig"]>=200)
+            })
+         }
+         else if(type==="withoutfruit"){
+            copyArr=copyArr.filter((item)=>{
+               return (!item["fruit"])
+            })
+         }else{
+            copyArr=copyArr.filter((item)=>{
+               return (item[type])
+            })
+         }
+      }
+      this.setState({Arr: copyArr})
+      setTimeout(()=>this.sortFunc(this.state.sortSelected),5)
+      
+   }
 
 
    sortFunc(e){
@@ -75,12 +111,14 @@ class Cakes extends Component {
             break;
 
          case "העדכני ביותר":
-            copyArr=[...updateArr]
+            copyArr.sort((a,b)=>b.id -a.id)
             break;
          default:
       }
 
       this.setState({Arr: copyArr})
+      this.setState({sortSelected: e})
+
    }
 
    render() {
