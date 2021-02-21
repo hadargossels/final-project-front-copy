7dbab6ba3b7f4bdacd435622e3b67d90502be789
Gilad <giladdek@post.bgu.ac.il>
2021-02-22 01:02:02 +0200

    changes

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,38 +2,19 @@ import React from "react";
 import { Route } from "react-router-dom";
 import { connect } from "react-redux";
 
-import { fetchCollectionsStartAsync } from "../../redux/shop/shop.actions";
+import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
 
 import CollectionsOverviewContainer from "../../components/collections-overview-All/collections-overview.container";
 import CollectionPageContainer from "../collection/collection.container";
 
-// import CollectionsOverviewAll from "./../../components/collections-overview-All/collections-overview-all.component";
-
-// const ShopPage = ({ match }) => (
-//   <div className="shop-page">
-//     <Route exact path={`${match.path}`} component={CollectionsOverviewAll} />
-//     <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-//   </div>
-// );
-
-// const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverviewAll);
-// const CollectionPageWithSpinner = WithSpinner(CollectionPage);
-
 class ShopPage extends React.Component {
-  // state = {
-  //   loading: true,
-  // };
-
-  // unsubscribeFromSnapshot = null;
-
   componentDidMount() {
-    const { fetchCollectionsStartAsync } = this.props;
-    fetchCollectionsStartAsync();
+    const { fetchCollectionsStart } = this.props;
+    fetchCollectionsStart();
   }
 
   render() {
     const { match } = this.props;
-    // const { loading } = this.state;
     return (
       <div className="shop-page">
         <Route
@@ -51,7 +32,7 @@ class ShopPage extends React.Component {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
